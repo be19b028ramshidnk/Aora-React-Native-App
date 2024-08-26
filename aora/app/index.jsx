820b7ac1f98bea1c659fe387e2f0283063505280
RAMSHID N K <ramshidnk007@gmail.com>
2024-08-26 21:40:09 +0530

    SignIn and SignUp completed

--- aora/app/index.jsx
+++ aora/app/index.jsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
+import { useGlobalContext } from '../context/GlobalProvider';
 export default function App() {
+  const { loading, isLogged } = useGlobalContext();
+  if (!loading && isLogged) return <Redirect href="/home" />;
+  
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView
@@ -12,7 +16,7 @@ export default function App() {
           height: "100%",
         }}
       >
-        <View className="w-full flex justify-center items-center h-full px-4">
+        <View className="w-full flex justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -42,7 +46,7 @@ export default function App() {
           
           <CustomButton
             title ="Continue with Email"
-            handlePress = {()=>{}}
+            handlePress = {()=>router.push('/sign-in')}
             containerStyles="w-full mt-7"
           />
 
